e8f43bfa11a83a56ccb237253f0c62c2350325fd
John Harrigan <jharriga@redhat.com>
2026-06-23 15:31:42 -0400

    Add OpenSSL stressor script with PCP metrics
    This script runs the OpenSSL stress test and creates a PCP archive with metrics. It includes configuration for workload parameters and logs the execution.

---
+++ openssl_example.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# Script which runs the OPENSSL stressor and creates a PCP
+# Archive with metrics as specified in the $pcp_conf_file
+# NOTE: the number of samples runs is hard-coded to 5
+###################################################################
+
+# Include the PCP Functions file
+source $PWD/pcp_functions.sh
+
+# PCP Dirs and Files
+# NOTE: pcp_archive_name is defined in Workload while-loop
+pcp_conf_file="$PWD/pcp_openssl.cfg"
+pcp_archive_dir="$PWD/archive.$(date +%Y%m%d%H%M%S)"
+
+# Define Timings
+delay=15             # Fixed DELAY between INNER-LOOP runs
+runtime=100          # Runtime duration for each workload run
+pcp_sample_rate=5    # Sample time for PMLOGGER recorded Metrics
+
+# Define workload
+##thread_cnt=$(nproc)                       # record number of cores
+thread_cnt=4
+pcp_archive_name="${thread_cnt}threads"
+runlog="${pcp_archive_dir}/${pcp_archive_name}.runlog"
+## use an array to build up cmdline with runtime args
+workload=( "openssl speed -evp sha256 -bytes 16384 -seconds 60 -multi ${thread_cnt}" )
+parsing=">>$runlog 2>&1"                # specific to $workload output
+exec_str="${workload[@]} ${parsing}"
+
+echo "Command: ${workload[0]}"
+echo "Workload: ${workload[@]}"
+echo " Exec string: ${exec_str}"
+echo "Number of threads for this set of runs: ${thread_cnt}"
+
+# Verify workload is available on the system
+if which "${workload[0]}" > /dev/null; then
+  echo "Command '${workload[0]}' found!"
+else
+  echo "Command '${workload[0]}' not found!"
+  exit 2
+fi
+
+#----------------------------------
+# Start PMLOGGER to create ARCHIVE
+pcp_verify $pcp_conf_file
+pcp_start $pcp_conf_file $pcp_sample_rate $pcp_archive_dir $pcp_archive_name
+
+# Loop - repeat Workload for 5 samples
+echo -n "Executing workload: Sample "
+for sample_ctr in {1..5}; do
+        echo -n "${sample_ctr} "
+        sleep $delay
+        return=$(eval "$exec_str")            # Run the Workload
+done
+
+echo                             # complete the new-line
+
+# Terminate PMLOGGER. Flush buffers by sending SIGUSR1 signal
+pcp_stop
+
+# Verify PCP ARCHIVE was created
+echo; echo "${pcp_archive_dir}"
+ls -l "${pcp_archive_dir}"
+
+echo; echo "------------------"
+
+echo "DONE"
